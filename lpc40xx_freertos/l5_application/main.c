faa8490c94997594a137a62f0c4033be0afc822c
Rutuja Shah <rutu.shah.26@gmail.com>
2019-10-31 12:27:59 -0700

    Fix compiler errors

--- lpc40xx_freertos/l5_application/main.c
+++ lpc40xx_freertos/l5_application/main.c
@@ -17,11 +17,13 @@ int main(void) {
   led0 = board_io__get_led0();
   led1 = board_io__get_led1();
 
-  xTaskCreate(blink_task, "led0", configMINIMAL_STACK_SIZE, (void *)&led0, PRIORITY_LOW, NULL);
-  xTaskCreate(blink_task, "led1", configMINIMAL_STACK_SIZE, (void *)&led1, PRIORITY_LOW, NULL);
+  xTaskCreate(blink_task, "led0", configMINIMAL_STACK_SIZE, (void *)&led0,
+              PRIORITY_LOW, NULL);
+  xTaskCreate(blink_task, "led1", configMINIMAL_STACK_SIZE, (void *)&led1,
+              PRIORITY_LOW, NULL);
 
-  // It is advised to either run the uart_task, or the SJ2 command-line (CLI), but not both
-  // Change '#if 0' to '#if 1' and vice versa to try it out
+  // It is advised to either run the uart_task, or the SJ2 command-line (CLI),
+  // but not both Change '#if 0' to '#if 1' and vice versa to try it out
 #if 0
   // printf() takes more stack space, size this tasks' stack higher
   xTaskCreate(uart_task, "uart", (512U * 8) / sizeof(void *), NULL, PRIORITY_LOW, NULL);
@@ -31,7 +33,8 @@ int main(void) {
 #endif
 
   puts("Starting RTOS");
-  vTaskStartScheduler(); // This function never returns unless RTOS scheduler runs out of memory and fails
+  vTaskStartScheduler(); // This function never returns unless RTOS scheduler
+                         // runs out of memory and fails
 
   return 0;
 }
@@ -39,31 +42,39 @@ int main(void) {
 static void blink_task(void *params) {
   const gpio_s led = *((gpio_s *)params);
 
-  // Warning: This task starts with very minimal stack, so do not use printf() API here to avoid stack overflow
+  // Warning: This task starts with very minimal stack, so do not use printf()
+  // API here to avoid stack overflow
   while (true) {
     gpio__toggle(led);
     vTaskDelay(500);
   }
 }
 
-// This sends periodic messages over printf() which uses system_calls.c to send them to UART0
+// This sends periodic messages over printf() which uses system_calls.c to send
+// them to UART0
 static void uart_task(void *params) {
   TickType_t previous_tick = 0;
   TickType_t ticks = 0;
 
   while (true) {
-    // This loop will repeat at precise task delay, even if the logic below takes variable amount of ticks
+    // This loop will repeat at precise task delay, even if the logic below
+    // takes variable amount of ticks
     vTaskDelayUntil(&previous_tick, 2000);
 
-    /* Calls to fprintf(stderr, ...) uses polled UART driver, so this entire output will be fully
-     * sent out before this function returns. See system_calls.c for actual implementation.
+    /* Calls to fprintf(stderr, ...) uses polled UART driver, so this entire
+     * output will be fully sent out before this function returns. See
+     * system_calls.c for actual implementation.
      *
      * Use this style print for:
      *  - Interrupts because you cannot use printf() inside an ISR
-     *  - During debugging in case system crashes before all output of printf() is sent
+     *  - During debugging in case system crashes before all output of printf()
+     * is sent
      */
     ticks = xTaskGetTickCount();
-    fprintf(stderr, "%u: This is a polled version of printf used for debugging ... finished in", (unsigned)ticks);
+    fprintf(stderr,
+            "%u: This is a polled version of printf used for debugging ... "
+            "finished in",
+            (unsigned)ticks);
     fprintf(stderr, " %lu ticks\n", (xTaskGetTickCount() - ticks));
 
     /* This deposits data to an outgoing queue and doesn't block the CPU
